cd61cb37dcf0b839024765bc5b4f029821ad5bb6
River Manoukian <river.m.phoenix@gmail.com>
2025-05-30 07:17:00 +1000

    added console app

---
+++ MSGSystems/MSGSevrice/Program.cs
@@ -0,0 +1,17 @@
+﻿var LISI = ("oops, looks like you have to log-in/sign-in");
+Console.WriteLine("Press any key to start...");
+Console.ReadKey();
+Thread.Sleep(2000);
+Console.WriteLine("Welcome to the Setro messaging service");
+Thread.Sleep(2000);
+Console.WriteLine(LISI);
+Thread.Sleep(2000);
+string[] LISIO = ["1-Sign-in", "2-Log-in"];
+foreach(var ppl in LISIO)
+{
+    Console.WriteLine(ppl);
+}
+Console.WriteLine("Please enter the number equevelent to what you want to do");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
